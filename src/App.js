93a2096387a1dc1c6dbaedd7ea1bcaca9058d64d
Ikecruz <66731231+Ikecruz@users.noreply.github.com>
2022-12-23 12:32:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import { TextInput } from '@mantine/core';
+
+export default function App() {
+  return (
+    <div>
+      <TextInput />
+    </div>
+  );
+}
